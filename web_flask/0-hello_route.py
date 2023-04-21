9c05b8db481b3d663b9f208c545b4be557deb58e
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2023-04-21 10:50:55 +0100

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,16 @@
-#!/usr/bin/python3
-"""Start the Flask web application.
-
-Application listens on 0.0.0.0, port 5000.
-
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
 Routes:
-    /:display 'Hello HBNB'
+    /: Displays 'Hello HBNB!'
 """
-
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Display 'Hello HBNB!'."""
+    """ Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
