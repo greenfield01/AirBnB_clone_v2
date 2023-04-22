c972c32627285a8969c4e7dcc046b147c70a8bc4
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2023-04-22 11:56:12 +0100

    route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
@@ -10,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Displays 'Hello HBNB!'."""
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
